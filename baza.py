032cec04188ba44137fb881f2d3573e174c58db4
Patryk Pieczarka <79982753+Patrico2906@users.noreply.github.com>
2021-05-19 12:58:35 +0200

    Add files via upload

---
+++ baza.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  19 12:55:24 2021
+
+@author: Patryk
+"""
+
+import sqlite3
+
+if __name__ == "__main__":
+
+    conn = sqlite3.connect('database.db')
+    print("BD otwarta")
+    
+    conn.execute('CREATE TABLE pracownicy (imienazwisko TEXT, nrpracownika TEXT, adres TEXT)')
+    print("Tabela utworzona")
+    
+    conn.close()
+    print("BD zamknieta")
+    
+    
+    conn = sqlite3.connect('database.db')
+    print("BD otwarta")
+    cur = conn.cursor()
+    
+    cur.execute("INSERT INTO pracownicy (imienazwisko, nrpracownika, adres) VALUES (?,?,?)",('Maniek Kamera','1','Warszawa') )
+    cur.execute("INSERT INTO pracownicy (imienazwisko, nrpracownika, adres) VALUES (?,?,?)",('Jarek Mata','2','Gdynia') )
+    conn.commit()
+    
+    cur.execute('SELECT * FROM pracownicy ORDER BY nrpracownika')
+    print(cur.fetchall())
+    
+    conn.close()
+    print("BD zamknieta")
